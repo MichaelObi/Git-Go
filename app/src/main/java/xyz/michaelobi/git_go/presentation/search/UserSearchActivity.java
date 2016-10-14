bb553fa2ec035b3f2c2b8a6af2851811cfad0efa
Michael Obi <ask4myk@gmail.com>
2016-10-14 07:46:33 +0100

    Part 5 Done: Running

--- app/src/main/java/xyz/michaelobi/git_go/presentation/search/UserSearchActivity.java
+++ app/src/main/java/xyz/michaelobi/git_go/presentation/search/UserSearchActivity.java
@@ -2,6 +2,14 @@ package xyz.michaelobi.git_go.presentation.search;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -9,10 +17,19 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import xyz.michaelobi.git_go.R;
 import xyz.michaelobi.git_go.data.remote.model.User;
+import xyz.michaelobi.git_go.injection.Injection;
+
+import static android.view.View.GONE;
 
 public class UserSearchActivity extends AppCompatActivity implements UserSearchContract.View {
 
     UserSearchContract.Presenter userSearchPresenter;
+    private UsersAdapter usersAdapter;
+    private SearchView searchView;
+    private Toolbar toolbar;
+    private ProgressBar progressBar;
+    private RecyclerView recyclerViewUsers;
+    private TextView textViewErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +38,76 @@ public class UserSearchActivity extends AppCompatActivity implements UserSearchC
         userSearchPresenter = new UserSearchPresenter(Injection.provideUserRepo(), Schedulers.io(), AndroidSchedulers
                 .mainThread());
         userSearchPresenter.attachView(this);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        textViewErrorMessage = (TextView) findViewById(R.id.text_view_error_msg);
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recycler_view_users);
+        usersAdapter = new UsersAdapter(null, this);
+        recyclerViewUsers.setAdapter(usersAdapter);
     }
 
     @Override
     public void showSearchResults(List<User> githubUserList) {
-
+        recyclerViewUsers.setVisibility(View.VISIBLE);
+        textViewErrorMessage.setVisibility(GONE);
+        usersAdapter.setItems(githubUserList);
     }
 
     @Override
     public void showError(String error) {
+        textViewErrorMessage.setVisibility(View.VISIBLE);
+        recyclerViewUsers.setVisibility(GONE);
+        textViewErrorMessage.setText(error);
+    }
 
+    @Override
+    protected void onDestroy() {
+        userSearchPresenter.detachView();
+
+        super.onDestroy();
     }
 
     @Override
     public void showLoading() {
-
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerViewUsers.setVisibility(GONE);
+        textViewErrorMessage.setVisibility(GONE);
     }
 
     @Override
     public void hideLoading() {
+        progressBar.setVisibility(View.GONE);
+        recyclerViewUsers.setVisibility(View.VISIBLE);
+        textViewErrorMessage.setVisibility(View.GONE);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_user_search, menu);
+        final MenuItem searchActionMenuItem = menu.findItem(R.id.menu_search);
+        searchView = (SearchView) searchActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!searchView.isIconified()) {
+                    searchView.setIconified(true);
+                }
+                userSearchPresenter.search(query);
+                toolbar.setTitle(query);
+                searchActionMenuItem.collapseActionView();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        searchActionMenuItem.expandActionView();
+        return true;
     }
 }
