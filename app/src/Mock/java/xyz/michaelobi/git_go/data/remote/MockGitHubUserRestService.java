2092b05935f68f92a8a80d644f4a4e3a7760c9c4
Michael Obi <ask4myk@gmail.com>
2016-10-15 13:23:20 +0100

    Finish UI tests

---
+++ app/src/Mock/java/xyz/michaelobi/git_go/data/remote/MockGitHubUserRestService.java
@@ -0,0 +1,8 @@
+package xyz.michaelobi.git_go.data.remote;
+
+/**
+ * Created by EP-Developer5 on 10/15/16.
+ */
+
+public class MockGitHubUserRestService {
+}
---
+++ app/src/androidTestMock/java/xyz/michaelob/git_go/presentation/search/UserSearchActivityTest.java
@@ -0,0 +1,29 @@
+package xyz.michaelob.git_go.presentation.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import xyz.michaelobi.git_go.R;
+import xyz.michaelobi.git_go.data.remote.model.User;
+import xyz.michaelobi.git_go.injection.Injection;
+import xyz.michaelobi.git_go.presentation.search.UserSearchContract;
+import xyz.michaelobi.git_go.presentation.search.UserSearchPresenter;
+import xyz.michaelobi.git_go.presentation.search.UsersAdapter;
+
+import static android.view.View.GONE;
+
+public class UserSearchActivityTest{
+    }
+}
